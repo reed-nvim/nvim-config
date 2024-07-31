aaa30035d59861cf510d0de4a54258fc7228b261
reed <reedemer@rapidintegrate.com>
2024-07-31 16:06:01 +0200

    updated lazygit and toggleterm to have colorscheme/highlights consisted with my telescope and fzf-lua config

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,20 +1,25 @@
 return {
-  'kdheepak/lazygit.nvim',
+  "kdheepak/lazygit.nvim",
   cmd = {
-    'LazyGit',
-    'LazyGitConfig',
-    'LazyGitCurrentFile',
-    'LazyGitFilter',
-    'LazyGitFilterCurrentFile',
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
   },
-  -- optional for floating window border decoration
   dependencies = {
-    'nvim-lua/plenary.nvim',
+    "nvim-lua/plenary.nvim",
   },
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
-  -- keys = {
-  --   { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
-  -- },
   lazy = false,
+  config = function()
+    -- Apply custom floating window highlights to match your color scheme
+    vim.cmd [[
+      highlight FloatBorder guifg=#2E3440 guibg=#2E3440
+      highlight NormalFloat guibg=#2E3440
+      highlight Normal guifg=#ECEFF4 guibg=#2E3440
+    ]]
+
+    -- Optionally, you can set up key mappings here if not using the 'keys' option
+    vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>", { desc = "Open LazyGit" })
+  end,
 }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,23 +5,29 @@ return {
   config = function()
     require("toggleterm").setup {
       -- Configuration here, or leave empty to use defaults
-      -- open_mapping = [[<c-a>]],
-      open_mapping = [[<leader><leader>]],
+      open_mapping = [[<c-a>]],
       start_in_insert = true,
       direction = "float",
       float_opts = {
         border = "curved",
         winblend = 4,
+        highlights = {
+          -- Custom highlights to match your theme
+          border = "FzfLuaBorder", -- Using the same border color as fzf-lua
+          background = "FzfLuaNormal", -- Match the background color with fzf-lua's normal background
+        },
       },
       highlights = {
-        -- highlights which map to a highlight group name and a table of it's values
-        -- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
+        -- Custom highlights to match the colors used in Telescope and fzf-lua
         FloatBorder = {
-          guifg = "#44475A",
-          guibg = "#44475A",
-          -- guifg = '#6272a4',
-          -- guibg = '#6272a4',
+          guifg = "#2E3440", -- Border color used in fzf-lua and Telescope
+          guibg = "#2E3440", -- Background color for the border, consistent with your theme
         },
+        NormalFloat = {
+          guifg = "#ECEFF4", -- Foreground color used in fzf-lua
+          guibg = "#2E3440", -- Background color consistent with fzf-lua
+        },
+        -- Additional customization can be added here to match other elements if needed
       },
     }
   end,
