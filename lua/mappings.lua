8a611a0bb39118aefba5677af35fae18b25aa97a
reed <reedemer@rapidintegrate.com>
2024-07-31 14:22:46 +0200

    updated mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,57 +2,57 @@
 --  See `:help vim.keymap.set()`
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>")
 -- Quick move back to insert mode
-vim.keymap.set('i', 'jk', '<Esc>')
+vim.keymap.set("i", "jk", "<Esc>")
 
 -- Remapping number increment and decrement key bindings
-vim.keymap.set({ 'n', 'x' }, '<leader>=', '<c-a>', { desc = 'increment number' }) -- remapping the incr and decr key maps to something more intuitive
-vim.keymap.set({ 'n', 'x' }, '<leader>-', '<c-x>', { desc = 'decrement number' }) -- remapping the incr and decr key maps to something more intuitive
+vim.keymap.set({ "n", "x" }, "<leader>=", "<c-a>", { desc = "increment number" }) -- remapping the incr and decr key maps to something more intuitive
+vim.keymap.set({ "n", "x" }, "<leader>-", "<c-x>", { desc = "decrement number" }) -- remapping the incr and decr key maps to something more intuitive
 
 -- Remapping semi colon to colon
-vim.keymap.set({ 'n', 'x' }, ';', ':FineCmdline<cr>', { desc = 'enter FineCmdline mode', silent = true })
+vim.keymap.set({ "n", "x" }, ";", ":FineCmdline<cr>", { desc = "enter FineCmdline mode", silent = true })
 
-vim.keymap.set({ 'n', 'x' }, '', '<c-^>', { desc = 'previous buffer' })
-vim.keymap.set({ 'n', 'x' }, '<leader>c', ':normal ~<cr>', { desc = 'toggle case' }) -- normal mode toggling of char case
-vim.keymap.set({ 'n', 'x' }, '<leader>\\', ':vsp<cr>', { desc = 'open vertical split' }) -- normal mode toggling of char case
-vim.keymap.set({ 'n', 'x' }, '<leader>/', ':normal gcc<cr>', { desc = 'toggle comment' }) -- normal mode toggling of char case
+vim.keymap.set({ "n", "x" }, ",", "<c-^>", { desc = "previous buffer" })
+vim.keymap.set({ "n", "x" }, "-", "<c-^>", { desc = "previous buffer" })
+vim.keymap.set({ "n", "x" }, "<leader>\\", ":vsp<cr>", { desc = "open vertical split" }) -- normal mode toggling of char case
+vim.keymap.set({ "n", "x" }, "<leader>/", ":normal gcc<cr>", { desc = "toggle comment" }) -- normal mode toggling of char case
 
 -- Opening Neotree
-vim.keymap.set('n', '<leader>el', ':Neotree toggle left<cr>', { desc = 'open Neotree on the left' })
-vim.keymap.set('n', '<leader>ef', ':Neotree toggle float<cr>', { desc = 'open Neotree in float position' })
-vim.keymap.set('n', '<leader>eg', ':Neotree float git_status<cr>', { desc = 'open git_status in floating Neotree' })
-vim.keymap.set('n', '<leader>eb', ':Neotree toggle bottom<cr>', { desc = 'open Neotree from the bottom of the screen' })
+vim.keymap.set("n", "<leader>el", ":Neotree toggle left<cr>", { desc = "open Neotree on the left" })
+vim.keymap.set("n", "<leader>ef", ":Neotree toggle float<cr>", { desc = "open Neotree in float position" })
+vim.keymap.set("n", "<leader>eg", ":Neotree float git_status<cr>", { desc = "open git_status in floating Neotree" })
+vim.keymap.set("n", "<leader>eb", ":Neotree toggle bottom<cr>", { desc = "open Neotree from the bottom of the screen" })
 
 -- vim.keymap.set('n', '<leader>th', require('fzf-lua').files, { desc = 'Fzf Files' })
 -- vim.keymap.set('n', '<c-P>', require('fzf-lua').files, { desc = 'Fzf Files' })
 
 -- Opening LazyGit
-vim.keymap.set('n', '<leader>lg', ':LazyGit<cr>', { desc = 'LazyGit' })
+vim.keymap.set("n", "<leader>lg", ":LazyGit<cr>", { desc = "LazyGit" })
 
 -- for tmux navigation
-vim.keymap.set({ 'n', 'x' }, '<c-h>', ':TmuxNavigateLeft<cr>')
-vim.keymap.set({ 'n', 'x' }, '<c-j>', ':TmuxNavigateDown<cr>')
-vim.keymap.set({ 'n', 'x' }, '<c-k>', ':TmuxNavigateUp<cr>')
-vim.keymap.set({ 'n', 'x' }, '<c-l>', ':TmuxNavigateRight<cr>')
-vim.keymap.set({ 'n', 'x' }, '<c-\\>', ':TmuxNavigatePrevious<cr>')
+vim.keymap.set({ "n", "x" }, "<c-h>", ":TmuxNavigateLeft<cr>")
+vim.keymap.set({ "n", "x" }, "<c-j>", ":TmuxNavigateDown<cr>")
+vim.keymap.set({ "n", "x" }, "<c-k>", ":TmuxNavigateUp<cr>")
+vim.keymap.set({ "n", "x" }, "<c-l>", ":TmuxNavigateRight<cr>")
+vim.keymap.set({ "n", "x" }, "<c-\\>", ":TmuxNavigatePrevious<cr>")
 
 -- Keybindings for bufferline
-vim.api.nvim_set_keymap('n', '<Tab>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-Tab>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>xo', ':BufferLineCloseOthers<CR>', { noremap = true, silent = true, desc = 'close all other visible buffers' })
-vim.api.nvim_set_keymap('n', '<leader>xb', ':bdelete<CR>', { noremap = true, silent = true, desc = 'close current buffer' })
+vim.api.nvim_set_keymap("n", "<Tab>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>xo", ":BufferLineCloseOthers<CR>", { noremap = true, silent = true, desc = "close all other visible buffers" })
+vim.api.nvim_set_keymap("n", "<leader>xb", ":bdelete<CR>", { noremap = true, silent = true, desc = "close current buffer" })
 vim.api.nvim_set_keymap(
-  'n',
-  '<leader>xr',
-  ':BufferLineCloseRight<CR>',
-  { noremap = true, silent = true, desc = 'close all visible buffers to the right of the current buffer' }
+  "n",
+  "<leader>xr",
+  ":BufferLineCloseRight<CR>",
+  { noremap = true, silent = true, desc = "close all visible buffers to the right of the current buffer" }
 )
 vim.api.nvim_set_keymap(
-  'n',
-  '<leader>xl',
-  ':BufferLineCloseLeft<CR>',
-  { noremap = true, silent = true, desc = 'close all visible buffers to the left of the current buffer' }
+  "n",
+  "<leader>xl",
+  ":BufferLineCloseLeft<CR>",
+  { noremap = true, silent = true, desc = "close all visible buffers to the left of the current buffer" }
 )
 
 -- Keybinds to make split navigation easier.
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,23 +1,24 @@
 return {
-  'akinsho/toggleterm.nvim', -- for versatile terminal integration (a better terminal in neovim)
-  version = '*',
+  "akinsho/toggleterm.nvim", -- for versatile terminal integration (a better terminal in neovim)
+  version = "*",
   lazy = false,
   config = function()
-    require('toggleterm').setup {
+    require("toggleterm").setup {
       -- Configuration here, or leave empty to use defaults
-      open_mapping = [[<c-a>]],
+      -- open_mapping = [[<c-a>]],
+      open_mapping = [[<leader><leader>]],
       start_in_insert = true,
-      direction = 'float',
+      direction = "float",
       float_opts = {
-        border = 'curved',
+        border = "curved",
         winblend = 4,
       },
       highlights = {
         -- highlights which map to a highlight group name and a table of it's values
         -- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
         FloatBorder = {
-          guifg = '#44475A',
-          guibg = '#44475A',
+          guifg = "#44475A",
+          guibg = "#44475A",
           -- guifg = '#6272a4',
           -- guibg = '#6272a4',
         },
