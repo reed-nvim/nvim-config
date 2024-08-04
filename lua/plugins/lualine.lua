7c8c912e1ed9e5cdeef75e89a24ac95c8271c5ee
reed <reedemer@rapidintegrate.com>
2024-08-04 12:52:41 +0200

    updated lualine theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,13 @@
 return {
   "nvim-lualine/lualine.nvim",
+  version = "*",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("lualine").setup {
       options = {
-        theme = "dracula",
+        -- theme = "dracula",
         -- theme = "horizon",
-        -- theme = "iceberg_dark",
+        theme = "iceberg_dark",
         -- theme = "material",
         -- theme = "molokai",
         -- theme = "nightfly",
