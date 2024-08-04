3f60c866d723f916c427121c682fa4fd6423dce4
reed <reedemer@rapidintegrate.com>
2024-08-04 12:52:25 +0200

    updated gitsigns config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -15,6 +15,17 @@ return {
       },
       attach_to_untracked = false,
       current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 200,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+      },
+      current_line_blame_formatter = " <author>, <author_time:%R>: <summary>",
+      signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+      numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = true, -- Toggle with `:Gitsigns toggle_linehl`
       on_attach = function(bufnr)
         local gitsigns = require "gitsigns"
 
