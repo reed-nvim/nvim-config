7dce99425e9415a9b9de6e0a5166937a59830ee6
reed <attack.titan.reed@gmail.com>
2024-08-30 15:07:02 +0200

    updated keymap to toggle neo column line length check

--- lua/plugins/neocolumn-nvim.lua
+++ lua/plugins/neocolumn-nvim.lua
@@ -4,6 +4,6 @@ return {
   config = function()
     require("NeoColumn").setup()
     -- require("NeoColumn").toggle_neocolumn()
-    vim.keymap.set("n", "<leader>h", "<cmd>ToggleNeoColumn<cr>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>ch", "<cmd>ToggleNeoColumn<cr>", { noremap = true, silent = true })
   end,
 }
