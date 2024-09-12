8a4e6fa6bbaa22c42adfd3a9896b6acde2bbadc1
reed <attack.titan.reed@gmail.com>
2024-09-12 20:59:04 +0200

    added neotree mapping to reveal and currently open file in the explore

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -20,6 +20,7 @@ vim.keymap.set({ "n", "x" }, "<leader>/", ":normal gcc<cr>", { desc = "toggle co
 -- vim.keymap.set("n", "<leader>el", ":Neotree toggle left<cr>", { desc = "open Neotree on the left" })
 -- vim.keymap.set("n", "<leader>eb", ":Neotree toggle bottom<cr>", { desc = "open Neotree from the bottom of the screen" })
 vim.keymap.set("n", "<leader>ef", ":Neotree toggle float<cr>", { desc = "open Neotree in float position" })
+vim.keymap.set("n", "<leader>er", ":Neotree reveal float<cr>", { desc = "reveal current File in Neotree" })
 -- vim.keymap.set("n", "<leader>eg", ":Neotree float git_status<cr>", { desc = "open git_status in floating Neotree" })
 
 -- Opening LazyGit
